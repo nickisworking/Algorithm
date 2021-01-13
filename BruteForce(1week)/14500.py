18a05edc05f219bb580a6db574e5077a447b92a0
JunhoBae999 <junho918@naver.com>
2021-01-13 15:11:47 +0900

    14500 clear

---
+++ BruteForce(1week)/14500.py
@@ -0,0 +1,51 @@
+import sys
+from copy import deepcopy
+
+
+maxval = 0
+
+def rotated(array_2d):
+    list_of_tuples = zip(*array_2d[::-1])
+    return [list(elem) for elem in list_of_tuples]
+
+def check(board, tet,n,m) :
+    global maxval
+    x_len = len(tet)
+    y_len = len(tet[0])
+    for startx in range(n) :
+        if startx + (x_len-1) >= n :
+            continue  
+        for starty in range(m) : 
+            sumval = 0
+            if starty+(y_len-1) >= m :
+                continue
+            for j in range(x_len) :
+                for i in range(y_len) :
+                    if tet[j][i] == 1 :
+                        sumval += board[startx+ j][starty+i] 
+            maxval = max(sumval,maxval)
+                    
+def solve() :
+    n , m = map(int,sys.stdin.readline().split())    
+    board = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
+
+    tet1 = [[1,1,1,1]]
+    tet2 = [[1,1],[1,1]]
+    tet3 = [[1,0],[1,0],[1,1]]
+    tet4 = [[1,0],[1,1],[0,1]]
+    tet5 = [[1,1,1],[0,1,0]]
+    tet6 = [[0,1],[1,1],[1,0]]
+    tet7 = [[0,1],[0,1],[1,1]]
+    tet8 = [[1,1],[1,0],[1,0]]
+    tet9 = [[1,1],[0,1],[0,1]]
+    tet = [tet1,tet2,tet3,tet4,tet5,tet6,tet7,tet8,tet9]
+
+    for t in tet :
+        for _ in range(4):
+            check(board,t,n,m)
+            t = rotated(t)
+
+    print(maxval)
+
+if __name__ == "__main__" :
+    solve()
--- BruteForce(1week)/testfiles.py
+++ BruteForce(1week)/testfiles.py
@@ -1,1 +1,15 @@
-print(-8//4)
+def rotated(array_2d):
+    list_of_tuples = zip(*array_2d[::-1])
+    return [list(elem) for elem in list_of_tuples]
+
+tet1 = [[1,1,1,1]]
+tet2 = [[1,1],[1,1]]
+tet3 = [[1,0],[1,0],[1,1]]
+tet4 = [[1,0],[1,1],[0,11]]
+tet5 = [[1,1,1],[0,1,0]]
+
+print(tet3)
+print("====================")
+
+tet3 = rotated(tet3)
+print(tet3)
