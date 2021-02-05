cf7d34a727afb3be81ccd54c44cf95563cb4508f
JunhoBae999 <junho918@naver.com>
2021-02-05 18:33:31 +0900

    commited

---
+++ DP/12896.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+def solve(n,scvs) :
+    pass
+
+if __name__ == "__main__" :
+    n = int(input())
+    scvs = list(map(int,input().split(" ")))
+
+    solve(n,scvs)
+
+    
+
+
+
+    
+    
--- DP/1495.py
+++ DP/1495.py
@@ -13,19 +13,6 @@ possible_list = []
 
 
 
-for i in range(2,N+1) :
-    
-        
-    for j in range(2,M+1) :
-        
-
-
-
-
-
-
-
-
 
 
 
